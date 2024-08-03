536287a8303a920d39378419001a97c77f459329
Lori Joy <lpeterson2@ec.rr.com>
2024-08-02 20:32:30 -0400

    Update script.js
    made answers bold and added a console log for trouble shooting

--- script.js
+++ script.js
@@ -16,11 +16,22 @@ function checkAnswer(answer) {
     if (answers[answer]) {
         result.textContent = 'Correct!';
         result.style.color = 'green';
+
+        result.style.fontWeight = 'bold';
+        result.style.fontSize = '1.2em'
+        
     } else {
         result.textContent = 'Wrong, try again!';
         result.style.color = 'red';
+        result.style.fontWeight = 'bold';
+        result.style.fontSize = '1.2em'
     }
 
+console.log("Result text set to:", result.textContent);
+console.log("Result color set to:", result.style.color);
+console.log("Result font weight set to:", result.style.fontWeight);
+console.log("Result font size set to:", result.style.fontSize);
+    
     setTimeout(() => {
         result.textContent = '';
     }, 2000);
